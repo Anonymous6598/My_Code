af6e2062ab6f7cce4d531c7fe9ed9a34496c7c60
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2024-07-31 19:12:50 +0200

    Update My_Code.py

--- My_Code/My_Code.py
+++ My_Code/My_Code.py
@@ -128,17 +128,14 @@ class Program(My_Code_window.Tk, My_Code_Interface.My_Code_Interface):
         if locale.getdefaultlocale()[0] == f"sr_RS":
             self.main_screen_exit: tkinter.messagebox = tkinter.messagebox.askyesno(title=f"излаз", message=f"желите да изађете?")
             if self.main_screen_exit: sys.exit()
-            else: pass
 
         elif locale.getdefaultlocale()[0] == f"ru_RU":
             self.main_screen_exit: tkinter.messagebox = tkinter.messagebox.askyesno(title=f"выход", message=f"желайте выйти?")
             if self.main_screen_exit: sys.exit()
-            else: pass
 			
         else:
             self.main_screen_exit: tkinter.messagebox = tkinter.messagebox.askyesno(title=f"exit", message=f"would you like to exit?")
             if self.main_screen_exit: sys.exit()
-            else: pass
         
 if __name__ == f"__main__":
     program: Program = Program()
