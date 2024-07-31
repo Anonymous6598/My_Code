2e844c2c70df07af6776a3563da0c86760973676
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2024-07-31 18:55:48 +0200

    Add files via upload

---
+++ My_Code/My_Code_AI_window.py
@@ -0,0 +1,40 @@
+from customtkinter import *
+import My_Code_AI, typing, asyncio, tkinter
+
+class AI_Window(CTkToplevel):
+
+	TITLE: typing.Final[str] = f"My Code AI assistant"
+	HEIGHT: typing.Final[int] = 300
+	WIDTH: typing.Final[int] = 525
+	ICON: typing.Final[str] = f"My Code icon.ico"
+	THEME: typing.Final[str] = f"system"
+
+	def __init__(self: typing.Self, *args, **kwargs) -> None:
+		CTkToplevel.__init__(self, *args, **kwargs)
+
+		self.title(self.TITLE)
+		self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+		self.resizable(False, False)
+		self.after(250, lambda: self.iconbitmap(self.ICON))
+
+		self.ai_window_textbox: CTkTextbox = CTkTextbox(master=self, height=265, width=524, corner_radius=0, fg_color=f"transparent")
+		self.ai_window_textbox.place(x=0, y=0)
+
+		self.ai_window_textbox.configure(state=f"disabled")
+
+		self.ai_window_entry: CTkEntry = CTkEntry(master=self, height=30, width=524, border_width=0, fg_color=f"transparent", placeholder_text=f"...")
+		self.ai_window_entry.place(x=0, y=269)
+
+		self.ai_window_entry.bind(f"<Return>", self.__response__)
+
+	@typing.override
+	def __response__(self: typing.Self, configure: str | None = None) -> None:
+		self.ai_window_entry_data: str = self.ai_window_entry.get()
+
+		self.ai_window_textbox.configure(state=f"normal")
+		self.query: str = asyncio.run(My_Code_AI.My_Code_LM().__response__(self.ai_window_entry_data))
+
+		self.ai_window_textbox.insert(tkinter.END, f"{self.query}\n", f"-1.0")
+		self.ai_window_textbox.configure(state=f"disabled")
+		self.ai_window_entry.delete(f"-1", tkinter.END)
+
---
+++ My_Code/My_Code_Interface.py
@@ -0,0 +1,27 @@
+import abc, typing
+
+class My_Code_Interface(abc.ABC):
+    
+    @abc.abstractmethod
+    def __run_code__(self: typing.Self, event: str | None = None) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __save_code__(self: typing.Self, event: str | None = None) -> None:
+        pass
+    
+    @abc.abstractmethod
+    def __open_code__(self: typing.Self, event: str | None = None) -> None:
+        pass
+    
+    @abc.abstractmethod
+    def __delete_code__(self: typing.Self, event: str | None = None) -> None:
+        pass
+    
+    @abc.abstractmethod
+    def __open_ai_window__(self: typing.Self, event: str | None = None) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __change_text_size__(self: typing.Self, event: str | None = None) -> None:
+        pass
