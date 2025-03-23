881874035ee81c4363e38c210c0875678a01b875
Виктор Илић <vikiilitch@outlook.com>
2025-03-23 01:55:54 +0100

    Update My_Code_AI.py

--- My_Code/Windows/My_Code_AI.py
+++ My_Code/Windows/My_Code_AI.py
@@ -2,8 +2,7 @@ import g4f, g4f.Provider, typing, My_Code_AI_interface
 
 class My_Code_LM(My_Code_AI_interface.My_Code_AI_interface):
     
-    LANGUAGE_MODEL: typing.Final[str] = f"gpt-4o-mini" 
-    PROVIDER: typing.Final[g4f.Provider] = g4f.Provider.DDG
+    LANGUAGE_MODEL: typing.Final[str] = f"gpt-4o"
     
     def __init__(self: typing.Self) -> None:
        self.language_model: str = self.LANGUAGE_MODEL
@@ -13,6 +12,6 @@ class My_Code_LM(My_Code_AI_interface.My_Code_AI_interface):
     def __response__(self: typing.Self, prompt: str) -> str:
         self.user_query: list[dict[str, str]] = [{f"role": f"user", f"content": prompt}]
         
-        self.response: str = g4f.ChatCompletion.create(model=self.language_model, provider=self.provider, messages=self.user_query)
+        self.response: str = g4f.ChatCompletion.create(model=self.language_model, messages=self.user_query)
 
         return self.response
