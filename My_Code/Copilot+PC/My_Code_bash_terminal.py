094ddb8599eca3bb35e9d0b2233a5751d3ef4c56
Виктор Илић <vikiilitch@outlook.com>
2025-05-06 20:58:37 +0000

    Add files via upload

---
+++ My_Code/Copilot+PC/My_Code_bash_terminal.py
@@ -0,0 +1,16 @@
+import customtkinter, tkterm, typing
+
+class My_Code_bash_terminal(customtkinter.CTkToplevel):
+    
+    TITLE: typing.Final[str] = f"My Code Bash window"
+    ICON: typing.Final[str] = f"my Code icon.ico"
+    
+    def __init__(self: typing.Self, *args, **kwargs):
+        customtkinter.CTkToplevel.__init__(self, *args, **kwargs)
+        
+        self.title(self.TITLE)
+        self.after(250, lambda: self.iconbitmap(self.ICON))
+
+        self.bash_terminal: tkterm.Terminal = tkterm.Terminal(self)
+        self.bash_terminal.shell = True
+        self.bash_terminal.pack(expand=True, fill=f"both")
