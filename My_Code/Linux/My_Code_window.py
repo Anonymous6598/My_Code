a1bd3b7fcdd35d6f477932e68af4bd1b90633abc
Виктор Илић <vikiilitch@outlook.com>
2025-05-14 00:05:25 +0200

    Delete My_Code directory

--- My_Code/Linux/My_Code_window.py
+++
@@ -1,6 +0,0 @@
-import customtkinter, tkinterdnd2,  typing
-
-class Window(customtkinter.CTk, tkinterdnd2.TkinterDnD.DnDWrapper):
-    def __init__(self: typing.Self, *args, **kwargs):
-        customtkinter.CTk.__init__(self, *args, **kwargs)
-        self.TkdndVersion = tkinterdnd2.TkinterDnD._require(self)
