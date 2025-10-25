90305bffb56d46531495e960984894b0870c0802
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 17:56:31 +0200

    Update My_Code_Interface.py

--- My_Code/Linux/My_Code_Interface.py
+++ My_Code/Linux/My_Code_Interface.py
@@ -17,3 +17,19 @@ class My_Code_Interface(abc.ABC):
     @abc.abstractmethod
     def __delete_code__(self: typing.Self, event: str | None = None) -> None:
         pass
+
+    @abc.abstractmethod
+    def __code_explanation__(self: typing.Self, event: str | None = None) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __create_code__(self: typing.Self, event: str | None = None) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __exit__(self: typing.Self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __fullscreen__(self: typing.Self) -> None:
+        pass
