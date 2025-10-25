66d7595ec9b3561a960ba3cdb2ce00e5e8f702ed
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 18:27:31 +0200

    Update My_Code_AI_window_interface.py

--- My_Code/Copilot+PC/My_Code_AI_window_interface.py
+++ My_Code/Copilot+PC/My_Code_AI_window_interface.py
@@ -3,5 +3,9 @@ import abc, typing
 class My_Code_AI_window_interface(abc.ABC):
 
    @abc.abstractmethod
-   def __response__(self: typing.Self, prompt: str) -> str:
+   def __response__(self: typing.Self, prompt: str) -> None:
+       pass
+
+   @abc.abstractmethod
+   def __audio_input__(self: typing.Self) -> None:
        pass
